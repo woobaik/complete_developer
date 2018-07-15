21ed257ea3b00b3136f9dfc3e9d82bbf80c56720
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-15 16:24:27 -0700

    group criteria

--- Block,Procs & Lamdas/block.rb
+++ Block,Procs & Lamdas/block.rb
@@ -11,3 +11,23 @@ class Array
     self
   end
 end
+
+
+class Array
+  def group_by_criteria
+    yes_group = []
+    no_group = []
+
+    self.each do |x|
+      if yield(x)
+        yes_group << x
+      else
+        no_group << x
+      end
+    end
+    return [yes_group, no_group]
+  end
+
+end
+
+# (1..10).to_a.group_by_criteria {|x| x.even? }
