55885b6898dbf252cf8eb1caaac2def594333466
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-16 23:09:17 -0700

    Resouces tasks

--- to_do/config/routes.rb
+++ to_do/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/destroy'
+
+  resources :tasks
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
