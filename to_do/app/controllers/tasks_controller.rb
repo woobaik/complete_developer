11437fc2a4f162faed8ea0fda39f5e5499b25ca2
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-17 09:14:03 -0700

    Create 'new', 'create' action

--- to_do/app/controllers/tasks_controller.rb
+++ to_do/app/controllers/tasks_controller.rb
@@ -1,11 +1,22 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "Your task has been posted."
+      redirect_to task_path
+    else
+      flash[:notice] = " There was something wrong with your request."
+      render 'new'
+    end 
   end
 
   def edit
