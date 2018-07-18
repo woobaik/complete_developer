af9a1cfb0b869bcf945dbccc803188ceff3b0cd0
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-17 21:30:40 -0700

    Create data on seed file, and User model

--- to_do/db/schema.rb
+++ to_do/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_214215) do
+ActiveRecord::Schema.define(version: 2018_07_18_042453) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_07_17_214215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- to_do/db/seeds.rb
+++ to_do/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Task.create({title: i.to_s + ' Hamie', description: 'I am hungry as hell'})
+
+end
