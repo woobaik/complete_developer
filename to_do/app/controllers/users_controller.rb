1228fa192a84cad09f98a482c9e900bdd4ed6a31
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-17 21:41:40 -0700

    Create 'create', 'new' action and installe BCRYPT

--- to_do/app/controllers/users_controller.rb
+++ to_do/app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Task.com"
+      redirect_to '/'
+    else
+      flash[:notice] = "There was something wrong adding you this time."
+      render 'new'
+    end
   end
 
   def edit
@@ -12,5 +21,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
