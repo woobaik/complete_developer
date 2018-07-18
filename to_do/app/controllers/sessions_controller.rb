000c4ab2700b2fb25f8068263532242f814e0c7d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-18 12:39:30 -0700

    Simple sessions for app

--- to_do/app/controllers/sessions_controller.rb
+++ to_do/app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome to Task.com #{user.username}"
+      redirect_to '/'
+    else
+      flash[:notice] = "There was something wrong with your login information"
+      render 'new'
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil if session[:user_id]
+    redirect_to '/'
+
   end
 end
